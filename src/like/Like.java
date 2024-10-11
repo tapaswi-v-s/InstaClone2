98aa0400a45e5c93167ca8ec1d21b30d1ff60628
The-shark <natashankatha@gmail.com>
2024-10-11 17:10:00 -0400

    add like post

---
+++ src/like/Like.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package like;
+
+/**
+ *
+ * @author kiara
+ */
+public class Like {
+    public static void main(String[] args) {
+        System.out.println("Like Post");
+    }
+}
