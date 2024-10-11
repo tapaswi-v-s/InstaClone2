531dabef6be8dabfd35fbdd0a01780f8dcfbb84e
Tapaswi <satyapanthi.t@northeastern.edu>
2024-10-11 17:10:08 -0400

    bug fixes

---
+++ src/bug_fixing/BugFixing.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bug_fixing;
+
+/**
+ *
+ * @author tapas
+ */
+public class BugFixing {
+    public static void main(String[] args) {
+        System.out.println("Bug Fixing");
+    }
+}
